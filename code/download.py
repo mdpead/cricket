d32ec458e096ce221dc13ad3c4a28c35cb7b5b41
Matthew Pead <mdpead@gmail.com>
2019-12-11 10:47:58 +0000

    Add simple download script

---
+++ code/download.py
@@ -0,0 +1,17 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+dfs = []
+for i in range(1, 3):
+
+    url = f'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=11;orderby=start;page={i};template=results;type' \
+          '=batting;view=innings;wrappertype=print'
+    page = requests.get(url)
+    html_table = BeautifulSoup(page.text, 'html.parser').find_all('table', class_='engineTable')[2]
+    df = pd.read_html(str(html_table), header=0)[0]
+    dfs.append(df)
+
+ds = pd.concat(dfs, axis=0)
+
+ds.to_pickle('..\\data\\batting_history.pkl')
